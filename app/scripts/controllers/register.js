4e4cfbbd8c2c6898bde4e9cb2ce5a1b040be0ee7
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-04 20:30:16 +0300

    Add simple registration

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -2,8 +2,42 @@
   'use strict';
 
   angular.module('jsNgWithFirebaseApp')
-    .controller('RegisterCtrl', function ($scope) {
+    .controller('RegisterCtrl', function ($scope, $firebaseSimpleLogin, FBURL, $window) {
+      var fbRef = new Firebase(FBURL);
+      $scope.errors = [];
+      $scope.simpleLogin = $firebaseSimpleLogin(fbRef);
+      $scope.registerUser = {
+        email: '1111',
+        password: '',
+        confirmPassword: ''
+      };
 
+      $scope.registerNewUser = function() {
+        var errors = [];
+        var user = $scope.registerUser;
+        if(!user.password.length) {
+          errors.push('Please specify password');
+        } else if(user.password !== user.confirmPassword) {
+          errors.push('Please confirm password correctly')
+        }
+        if(user.email === '') {
+          errors.push('Please enter email');
+        }
+
+
+        if(errors.length) {
+          $scope.errors = errors;
+          return;
+        }
+
+        $scope.simpleLogin
+          .$createUser($scope.registerUser.email, $scope.registerUser.password)
+          .then(function(user){
+            $window.location.href = '/#/home'
+          }, function(error) {
+
+          });
+      }
     });
 })(window.angular);
 
