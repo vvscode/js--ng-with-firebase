6243cc48caa620d1da12b53b08b9b8f89967a828
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-04 11:57:08 +0300

    Initial yeoman-angular generator setup

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -24,11 +24,12 @@ module.exports = function(config) {
       // bower:js
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/firebase/firebase.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
