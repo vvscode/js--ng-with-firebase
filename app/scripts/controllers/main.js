9bfd22bb64959cb5eed98dcc2ec76dd355d55497
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-04 13:02:40 +0300

    On change item

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,10 +19,40 @@ angular.module('jsNgWithFirebaseApp')
 
     messagesRef.on('child_added', function(snapshot) {
       $timeout(function() { // $timeout used instead of $digest/$apply
-        var value = snapshot.val();
-        $scope.messages.push(value);
+        $scope.messages.push(getSnapshotsMessage(snapshot));
       });
     });
+
+    messagesRef.on('child_changed', function(snapshot) {
+      $timeout(function() { // $timeout used instead of $digest/$apply
+        var value = snapshot.val();
+        var message = findMessageByName(snapshot.name());
+        if(message) {
+          message.text = value.text;
+          message.user = value.user;
+        } else {
+          $scope.messages.push(getSnapshotsMessage(snapshot));
+        }
+      });
+    });
+
+    function getSnapshotsMessage(snapshot){
+      var value = snapshot.val();
+      return {
+        text: value.text,
+        user: value.user,
+        name: snapshot.name()
+      };
+    }
+
+    function findMessageByName(name) {
+      for(var i = 0; i < $scope.messages.length; i++) {
+        var currentMessage = $scope.messages[i];
+        if(name === currentMessage.name) {
+          return currentMessage;
+        }
+      }
+    }
 
     $scope.sendMessage = function() {
       messagesRef.push({
