0c1e66ef23058e19b02a21ff0a9a2e4bf96c11fb
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-04 12:09:21 +0300

    Firebase "First updates"

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,4 @@
+/* global Firebase */
 'use strict';
 
 /**
@@ -8,10 +9,24 @@
  * Controller of the jsNgWithFirebaseApp
  */
 angular.module('jsNgWithFirebaseApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    var rootRef = new Firebase('https://intense-torch-8090.firebaseio.com/');
+    var messageRef = rootRef.child('message');
+
+    $scope.setMessage = function() {
+      messageRef.set({
+        user: 'VVS',
+        text: 'Hi!'
+      })
+    };
+
+    $scope.updateMessage = function() {
+      messageRef.update({
+        text: 'Bye'
+      });
+    };
+
+    $scope.deleteMessage = function() {
+      messageRef.remove();
+    }
   });
