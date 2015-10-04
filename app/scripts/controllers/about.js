a365e22e79e94f96be54194d1661cadd153f3864
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-04 11:54:41 +0300

    Yo-angular generator init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsNgWithFirebaseApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jsNgWithFirebaseApp
+ */
+angular.module('jsNgWithFirebaseApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsNgWithFirebaseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jsNgWithFirebaseApp
+ */
+angular.module('jsNgWithFirebaseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
